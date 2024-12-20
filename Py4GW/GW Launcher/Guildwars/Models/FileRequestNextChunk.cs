d1e370f5cc446b2e2fb217a44e466627327e73bd
ShivaTheLynch <lvo.vanoostrum@gmail.com>
2024-12-20 01:28:38 +0100

    Chage all

--- Py4GW/GW Launcher/Guildwars/Models/FileRequestNextChunk.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GW_Launcher.Guildwars.Models;
-internal readonly struct FileRequestNextChunk
-{
-    public short Field1 { get; init; }
-    public short Field2 { get; init; }
-    public uint Field3 { get; init; }
-}
